abe7161878958256e876932393e5c27501ffd0c6
AbdullahKhan-786 <abdullahawaiskhan2511@gmail.com>
2025-07-20 12:48:07 +0500

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,13 @@
+a=int(input('enter your first number'))
+b=int(input('enter your second number'))
+c=str(input('enter the mathematical operation you want to perform'))
+if (c=='+'):
+      print(a+b)
+elif (c=='-'):
+      print(a-b)
+elif (c=='*'):
+      print(a*b)
+elif  (c=='/'):
+      print(a/b)
+else:
+      print('wrong operation')
