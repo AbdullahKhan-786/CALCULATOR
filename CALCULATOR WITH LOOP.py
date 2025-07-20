303d2475fb18f8d8b7ea3ff23c0d3ddc0c6d6843
AbdullahKhan-786 <abdullahawaiskhan2511@gmail.com>
2025-07-20 12:40:19 +0500

    Add files via upload

---
+++ CALCULATOR WITH LOOP.py
@@ -0,0 +1,16 @@
+a=int(input('Please enter your first number'))
+b=int(input('Please enter your second number'))
+while True:
+    def add(a,b):
+     print(a+b)
+    add(a,b)
+    def subtract(a,b):
+     print(a-b)
+    subtract(a,b)
+    def multiply(a,b):
+     print(a*b)
+    multiply(a,b)
+    def divide(a,b):
+     print(a/b)
+    divide(a,b)
+    break
