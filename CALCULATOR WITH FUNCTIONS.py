a6d5ea64b44601cbffe4daf67cb58a4642a53628
AbdullahKhan-786 <abdullahawaiskhan2511@gmail.com>
2025-07-20 12:36:59 +0500

    Add files via upload

---
+++ CALCULATOR WITH FUNCTIONS.py
@@ -0,0 +1,14 @@
+a=int(input('Please enter first number'))
+b=int(input('Please enter second number'))
+def add(a,b):
+ print(a+b)
+add(a,b)
+def subtract(a,b):
+ print(a-b)
+subtract(a,b)
+def multiply(a,b):
+ print(a*b)
+multiply(a,b)
+def divide(a,b):
+ print(a/b)
+divide(a,b)
